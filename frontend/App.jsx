3ca3be5ee7f3a48ff00e647a1fd1982b9f158804
Aditya Mahajan <mahajan2005aditya@gmail.com>
2026-04-19 11:50:55 +0530

    refactor: Overuse of !important Declarations in CSS (#84)
    * REFACTOR: Excessive useState Hooks in React Components
    
    * refactor: Overuse of !important Declarations in CSS
    
    ---------
    
    Co-authored-by: Esha Jha <149310101+Eshajha19@users.noreply.github.com>

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -24,6 +24,7 @@ import useNotifications from "./Notifications";
 import { auth, db, onSnapshot, doc, isFirebaseConfigured } from "./lib/firebase";
 
 import "./App.css";
+import "./themes/sunlight.css";
 
 /* ---------------- LANGUAGE options ---------------- */
 
